312365201954a2e2f20e1f60748da715631e754c
William Soukkachang <WSoukkachang@yahoo.com>
2019-04-30 21:57:07 -0500

    Updated  dependencies. Added reducers and actions. Working on CharacterListView.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./reducers";
 // needed dependancies
 // applyMiddleware from redux
@@ -11,10 +11,12 @@ import rootReducer from "./reducers";
 // logger from redux-logger
 // rootReducer from ./reducers
 
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
 const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
-);
+  rootReducer, applyMiddleware(thunk, logger)
+  );
 
 ReactDOM.render(
   <Provider store={store}>
